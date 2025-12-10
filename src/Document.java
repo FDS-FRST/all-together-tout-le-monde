35e5e851ed8a926871097e22af40df5d1fd24ad8
whaugutin <whitchyaugustin@gmail.com>
2025-12-10 17:31:27 -0500

    Implementing the class of Document

---
+++ src/Document.java
@@ -0,0 +1,47 @@
+/**
+ * Une classe qui présente un document avec un numéro d'identification, un titre et l'année de publication
+ */
+public class Document {
+    private int id;
+    private String titre;
+    private int anneePublication;
+
+    public Document(int id, String titre, int anneePublication) {
+        this.id = id;
+        this.titre = titre;
+        this.anneePublication = anneePublication;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAnneePublication() {
+        return anneePublication;
+    }
+
+    public void setAnneePublication(int anneePublication) {
+        this.anneePublication = anneePublication;
+    }
+
+    @Override
+    public String toString() {
+        return "Document{" +
+                "id=" + id +
+                ", titre='" + titre + '\'' +
+                ", anneePublication=" + anneePublication +
+                '}';
+    }
+}
