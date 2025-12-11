2d430fb572e68b1d6772b39b07b4517e49dcd9e8
whaugutin <whitchyaugustin@gmail.com>
2025-12-11 17:40:32 -0500

    Refactor code:  - Create a catalogue of all Documents (dvds and livres)  - Implementation of afficheCatalogue's method  - Implementation of rechercheParId's method for the Map that have all documents  (All in the  Main class)

--- src/main/java/ht/mbds/frst/java/Main.java
+++ src/main/java/ht/mbds/frst/java/Main.java
@@ -3,7 +3,10 @@ package ht.mbds.frst.java;
 import ht.mbds.frst.java.data.Repository;
 import ht.mbds.frst.java.models.Document;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void afficheDocument(List<Document> documents) {
@@ -13,11 +16,36 @@ public class Main {
         }
     }
 
+    public static void afficheCataloge(List<Document> documents) {
+        afficheDocument(documents);
+    }
+
+    public static Document rechercherParId (Map<Integer, Document> documents, int id) {
+        for (Map.Entry<Integer, Document> entry : documents.entrySet()) {
+            if (entry.getKey() == id) {
+                return entry.getValue();
+            }
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
         var livres = Repository.getLivres();
         afficheDocument(livres);
-
+        System.out.println();
         var dvds = Repository.getDvds();
         afficheDocument(dvds);
+
+        List<Document> catalogue = new ArrayList<Document>(livres);
+        catalogue.addAll(dvds);
+        afficheCataloge(catalogue);
+
+        Map<Integer, Document> allDocuments = new HashMap<>();
+        for (Document doc : catalogue) {
+            allDocuments.put(doc.getId(), doc);
+        }
+
+
+
     }
 }
