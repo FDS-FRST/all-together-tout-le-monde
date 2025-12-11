c21e88b08bf21a8a05c9e135f1a6ec492f937d5d
whaugutin <whitchyaugustin@gmail.com>
2025-12-11 16:01:01 -0500

    Implementation of the Dvd's class, Implementation of a method to generate 50 random Dvds in Repository's class and a for loop to display the 5 first DVDs

---
+++ src/main/java/Dvd.java
@@ -0,0 +1,6 @@
+public class Dvd extends Document {
+
+    public Dvd(int id, String titre, int anneePublication) {
+        super(id, titre, anneePublication);
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,5 +5,13 @@ public class Main {
         for (int i = 0; i < Math.min(5, docs.size()); i++) {
             System.out.println(docs.get(i));
         }
+
+        var dvds = Repository.getDvds();
+        System.out.println("Total DVDs: " + dvds.size());
+        for (int i = 0; i < Math.min(5, dvds.size()); i++) {
+            System.out.println(dvds.get(i));
+        }
+
+        //Dvd dvd1 = new Dvd("")
     }
 }
