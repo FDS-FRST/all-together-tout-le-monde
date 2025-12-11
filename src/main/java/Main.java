b1cb41048b685f7670401a83c0e0c9d1ca2cd577
whaugutin <whitchyaugustin@gmail.com>
2025-12-11 16:33:33 -0500

    add empty SOUT

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,19 +5,18 @@ public class Main {
         for (int i = 0; i < Math.min(5, docs.size()); i++) {
             System.out.println(docs.get(i));
         }
-
+        System.out.println();
         var livres = Repository.getLivres();
         System.out.println("Total Livres: " + livres.size());
         for (int i = 0; i < Math.min(5, livres.size()); i++) {
             System.out.println(livres.get(i));
         }
 
+        System.out.println();
         var dvds = Repository.getDvds();
         System.out.println("Total DVDs: " + dvds.size());
         for (int i = 0; i < Math.min(5, dvds.size()); i++) {
             System.out.println(dvds.get(i));
         }
-
-        //Dvd dvd1 = new Dvd("")
     }
 }
