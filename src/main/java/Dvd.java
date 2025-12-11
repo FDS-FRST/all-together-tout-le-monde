5b108000c279bd57bbe022aa047fdfe0ee038dfd
whaugutin <whitchyaugustin@gmail.com>
2025-12-11 16:12:35 -0500

    Change visibility of the attributes in Document and Rewriting toString in Dvd

--- src/main/java/Dvd.java
+++ src/main/java/Dvd.java
@@ -3,4 +3,13 @@ public class Dvd extends Document {
     public Dvd(int id, String titre, int anneePublication) {
         super(id, titre, anneePublication);
     }
+
+    @Override
+    public String toString() {
+        return "Dvd{" +
+                "id=" + id +
+                ", titre='" + titre + '\'' +
+                ", anneePublication=" + anneePublication +
+                '}';
+    }
 }
